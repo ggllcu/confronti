fed7898dc48c77a3b03e489e4a1d36ff0231f964
Luca Guglielmi <87c21@live.it>
2024-01-04 18:15:01 +0100

    Removing image from chi siamo

--- src/routes/chi-siamo/+page.svelte
+++ src/routes/chi-siamo/+page.svelte
@@ -7,7 +7,6 @@
 </script>
  
 <div>
-  <Img src="https://placehold.co/1200x800" alt="sample 1" />
   <h1 class="mb-2 text-6xl font-bold tracking-tight text-gray-900 dark:text-white">{data.story?.content.title}</h1>
   <div class="mb-3 font-normal text-gray-700 dark:text-gray-400 leading-tight rich-text">{@html resolvedRichText}</div>
   <p></p>
